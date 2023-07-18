4688764937c11b5ae132238810fb2577aaa62f88
Kyle Khai Tran <kylekhai04@gmail.com>
2023-07-18 18:35:07 -0400

    Add color/UI modifications to buttons/div/document

--- script.js
+++ script.js
@@ -20,16 +20,29 @@ let computerWins = 0;
 // Creating buttons for UI
 const rockButton = document.createElement("button");
 rockButton.textContent = "ROCK";
+rockButton.style.color = "white";
+rockButton.style.backgroundColor = "black";
+rockButton.style.width = "100px";
+rockButton.style.height = "50px";
 
 const paperButton = document.createElement("button");
 paperButton.textContent = "PAPER";
+paperButton.style.color = "white";
+paperButton.style.backgroundColor = "black";
+paperButton.style.width = "100px";
+paperButton.style.height = "50px";
 
 const scissorsButton = document.createElement("button");
 scissorsButton.textContent = "SCISSORS";
+scissorsButton.style.color = "white";
+scissorsButton.style.backgroundColor = "black";
+scissorsButton.style.width = "100px";
+scissorsButton.style.height = "50px";
 
 // Creating div for UI
 const divResult = document.createElement("div");
 const divCounter = document.createElement("div");
+divCounter.style.fontWeight = "bold";
 
 // Adding event listeners
 rockButton.addEventListener("click", function () {
@@ -43,6 +56,18 @@ paperButton.addEventListener("click", function () {
 scissorsButton.addEventListener("click", function () {
   playRound("SCISSORS");
 });
+
+//Creating center for buttons
+const centerButtons = document.querySelectorAll("button");
+
+// Center the choice buttons
+centerButtons.forEach((button) => {
+  button.style.display = "block";
+  button.style.margin = "0 auto";
+});
+
+// Apply background color to the document
+document.body.style.backgroundColor = "lightgreen";
 
 // Updating result display
 function updateDisplay(result) {
